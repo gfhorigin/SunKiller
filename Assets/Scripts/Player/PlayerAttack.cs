ee21fbd2787da9a2182376e7ec8878d26fca361a
gfhorigin <98154676+gfhorigin@users.noreply.github.com>
2024-07-03 17:45:57 +0700

    0.0.0.5
    -добавлена анимация стояния на месте
    - добавлен счетчик комбо

--- Assets/Scripts/Player/PlayerAttack.cs
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class PlayerAttack : MonoBehaviour
 {
@@ -15,8 +17,24 @@ public class PlayerAttack : MonoBehaviour
     public int Damage;
     public Animator animator;
 
+    public int ComboCounter;
+    public float ComboTimer ;
+    public float ComboTimerStart = 2f;
+    public Text ComboText;
+
     void Update()
     {
+        if(ComboTimer > 0)
+        {
+            ComboTimer-= Time.deltaTime;
+        }
+        else
+        {
+            ComboCounter = 0;
+            ComboText.text = " ";
+        }
+
+
         if(AttackCooldown <= 0)
         {
             if(Input.GetMouseButton(0))
@@ -34,6 +52,10 @@ public class PlayerAttack : MonoBehaviour
     }
     void Attack()
     {
+        ComboCounter +=1;
+        ComboTimer = ComboTimerStart;
+        ComboText.text = ComboCounter.ToString();
+
         Collider2D[] enemies = Physics2D.OverlapCircleAll(AttackPos.position,AttackRange, Enemy);
         for (int i = 0; i < enemies.Length; i++)
         {
